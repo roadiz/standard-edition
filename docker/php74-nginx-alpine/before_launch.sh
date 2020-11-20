657d654700c413b7a0b388a08aa9f54415680eff
Ambroise Maupate <ambroise@rezo-zero.com>
2020-11-20 16:37:43 +0100

    Export env vars for async usage

--- docker/php74-nginx-alpine/before_launch.sh
+++ docker/php74-nginx-alpine/before_launch.sh
@@ -1,7 +1,11 @@
+printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export (SYMFONY|APP|ROADIZ|MYSQL|JWT|MAILER|SOLR|VARNISH)" > /var/www/html/project_env.sh
+
 # Fix volume permissions
 /bin/chown -R www-data:www-data /var/www/html/files;
 /bin/chown -R www-data:www-data /var/www/html/web;
 /bin/chown -R www-data:www-data /var/www/html/app;
 
+/bin/chmod +x /var/www/html/project_env.sh;
+
 # Uncomment following line to enable automatic migration for your theme at each docker start
-# /usr/bin/sudo -u www-data bin/roadiz themes:migrate -n Base
+# /usr/bin/sudo -u www-data /var/www/html/project_env.sh; bin/roadiz themes:migrate -n Base
