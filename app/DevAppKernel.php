c4012c7b28bed2a4c99f09e54f9c6de15c5eaca6
Ambroise Maupate <ambroise@rezo-zero.com>
2021-02-16 16:31:30 +0100

    Typed arguments

--- app/DevAppKernel.php
+++ app/DevAppKernel.php
@@ -6,21 +6,17 @@ declare(strict_types=1);
  */
 class DevAppKernel extends AppKernel
 {
-    /**
-     * @var string
-     */
-    private $appName;
+    private string $appName;
 
     /**
      * @param string $environment
-     * @param boolean $debug
+     * @param bool $debug
      * @param bool $preview
      * @param string $appName
      */
-    public function __construct($environment, $debug, $preview = false, $appName = "roadiz_dev")
+    public function __construct(string $environment, bool $debug, bool $preview = false, string $appName = "roadiz_dev")
     {
         parent::__construct($environment, $debug, $preview);
-
         $this->appName = $appName;
     }
 
